c1b7190627aa3ac84bb6dc3d6db7c17524dd80da
Peter Lazar <peterlazar1993@gmail.com>
2023-03-10 15:59:34 +0100

    add scrollTo example

--- App.tsx
+++ App.tsx
@@ -6,31 +6,14 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import WebView from 'react-native-webview';
-import Animated from 'react-native-reanimated';
-
-const AnimatedWebView = Animated.createAnimatedComponent(WebView);
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import ScrollExample from './scroll_events/ScrollEvents';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <AnimatedWebView
-        source={{uri: 'https://docs.swmansion.com/react-native-reanimated/'}}
-      />
-    </SafeAreaView>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <ScrollExample />
+    </GestureHandlerRootView>
   );
 }
 
